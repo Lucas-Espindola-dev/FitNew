70eeaf261b72d6230ec6ce631c55cf8aa78cddbb
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-09 08:51:34 -0300

    Structuring views.py

--- fitnew/views.py
+++ fitnew/views.py
@@ -4,7 +4,7 @@ from django.views.generic import ListView, CreateView
 
 class ExercisesListView(ListView):
     model = Exercises
-    context_object_name = 'Exercises'
+    context_object_name = 'pages'
 
 
 class ExercisesCreateView(CreateView):
