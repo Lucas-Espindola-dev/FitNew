7f1d32119514a91d832fd423a1e3e08c68f78b96
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-09 08:56:24 -0300

    Refactoring and creating home page view

--- fitnew/views.py
+++ fitnew/views.py
@@ -1,11 +1,12 @@
 from fitnew.models import Exercises
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView
+from django.shortcuts import render
 
+
+def home(request):
+    return render(request, 'fitnew/pages/home.html')
 
 class ExercisesListView(ListView):
     model = Exercises
     context_object_name = 'pages'
 
-
-class ExercisesCreateView(CreateView):
-    model = Exercises
