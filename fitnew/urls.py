f2baaf41a818367a2bd81e5fc6e1a2f0940ddead
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-16 09:38:35 -0300

    Creating media root

--- fitnew/urls.py
+++ fitnew/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from fitnew import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 app_name = "fitnew"
@@ -7,4 +9,4 @@ app_name = "fitnew"
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('exercicios/', views.ExercisesListView.as_view(), name='exercises')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
