16a794e7a56da3536024a858accbe887293db08d
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-26 14:57:01 -0300

    Correcting views paths

--- fitnew/urls.py
+++ fitnew/urls.py
@@ -8,6 +8,6 @@ app_name = "fitnew"
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('exercicios/', views.ExercisesListView.as_view(), name='exercises'),
-    path('exercícios/<int:muscular_groups_id>', views.MuscularGroupsListView.as_view(), name='muscular_groups')
+    path('exercicios/', views.MuscularGroupsListView.as_view(), name='muscular_groups'),
+    path('exercícios/<int:muscular_groups_id>', views.ExercisesListView.as_view(), name='exercises')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
